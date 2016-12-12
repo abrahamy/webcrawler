6515f157a67238598914285bdf942f6847bc4860
Abraham Yusuf <aaondowasey@gmail.com>
2016-12-12 14:17:38 +0100

    implemented full text search on the archive model

--- webcrawler/items.py
+++ webcrawler/items.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 
-import datetime, scrapy
-from peewee import CharField, DateTimeField, Model
+import datetime, json, scrapy
+from peewee import CharField, DateTimeField, Model, SelectQuery
 from playhouse.db_url import connect
-from playhouse.postgres_ext import BinaryJSONField, TSVectorField
+from playhouse.postgres_ext import BinaryJSONField, TSVectorField, Match
+from playhouse.shortcuts import model_to_dict
 from scrapy.utils.project import get_project_settings
 
 
@@ -45,6 +46,42 @@ class Archive(Model):
             self.create_table(True)
 
         return super().save(**kwargs)
+    
+    def fulltext_search(self, term, page_number=1, items_per_page=20, return_json=True):
+        '''
+        Execute a full text search query on the model and return a paginated result
+        of matching records in JSON format or as model instances
+        '''
+        results = Archive.select().where(
+            Match(Archive.search, term) |
+            Match(Archive.metadata['title'], term) |
+            Match(Archive.metadata['description'], term)
+        ).paginate(page_number, items_per_page)
+
+        if return_json:
+            res
+        
+        return results
+    
+    def to_json(self, query=None):
+        '''
+        Return the list of models in the query as a JSON array if a SelectQuery
+        is given otherwise return the current model instance as a JSON object
+        @TODO: clean the metadata field
+        '''
+        to_dict = lambda m: model_to_dict(m, only=['url', 'metadata'])
+
+        if not query:
+            return json.dumps(to_dict(self))
+        
+        if type(query) is not SelectQuery:
+            raise ValueError
+        
+        results = []
+        for model in query:
+            results.append(to_dict(model))
+        
+        return json.dumps(results)
     
     class Meta:
         database = get_db()
