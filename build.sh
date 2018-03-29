95f587b46525f9cd9846323fde8ecd484c0ee8a0
Abraham Yusuf <aaondowasey@gmail.com>
2018-03-29 19:21:06 +0100

    integrating Celery and Scrapyd for spider management

--- build.sh
+++ build.sh
@@ -1,20 +1,17 @@
 #!/bin/sh
 
 prepare_build() {
+    echo "exporting required variables..."
+
+    export RABBITMQ_PASSWORD=$(pwgen 24 1)
+    export MYSQL_PASSWORD=$(pwgen 24 1)
+
     echo "preparing build..."
-    echo "creating docker-compose.yml from sample file..."
-
+    
     cp docker-compose.sample.yml docker-compose.yml
-    
-    echo "generating mysql password..."
-    
-    local mysql_password=$(pwgen 24 1)
-    local placeholder=some-random-gibberish
 
-    echo "mysql password generated! password is: $mysql_password"
-    echo "replacing password placeholder with new password..."
-
-    sed -i '' s/$placeholder/$mysql_password/g docker-compose.yml
+    mkdir scrapyd
+    cp scrapyd_Dockerfile scrapyd/Dockerfile
 }
 
 set -x
