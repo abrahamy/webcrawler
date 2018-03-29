10a64140e213f8aa0ab4225935005e952c620f28
Abraham Yusuf <aaondowasey@gmail.com>
2018-03-29 21:55:47 +0100

    fix

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -2,11 +2,8 @@
 
 cd /home/api
 
-# Wait for database container to be ready
-. /wait-for-it.sh ${MYSQL_DATABASE_HOST}:3306 -t 40
-
-# Wait for rabbitmq container to be ready
-. /wait-for-it.sh ${RABBITMQ_HOST}:5672 -t 20
+# wait for 3 minutes for mysql and rabbitmq to come up
+sleep 180
 
 # Start API server
 uwsgi --yaml uwsgi.yml
